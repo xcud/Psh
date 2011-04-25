1a15b226a2ef4953d8a4789436d03c10e2c19db7
xcud <ben@vierck.us>
2011-04-25 10:13:57 -0500

    add get-psowner

---
+++ Get-PSOwner.psm1
@@ -0,0 +1,24 @@
+# Get-PSOwner.psm1
+# Author:       xcud
+#
+# PS Example> ps chrome* | get-psowner
+# Name                                    Handle                                  Owner
+# ----                                    ------                                  -----
+# chrome.exe                              1940                                    ben
+# chrome.exe                              5892                                    ben
+
+function Get-PSOwner
+{	
+	PROCESS {	
+		gwmi Win32_Process -Filter ("Handle={0}" -f $_.id ) | 
+			% { Add-Member `
+				-InputObject $_ `
+				-MemberType NoteProperty `
+				-Name Owner `
+				-Value ($_.GetOwner().User) `
+				-PassThru } | 
+			select Name, Handle, Owner
+	}
+}
+
+Export-ModuleMember Get-PSOwner

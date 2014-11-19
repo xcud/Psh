c0755ea45c2c821d848ddca8e028879d62c1a326
Ben Vierck <me@xcud.com>
2014-11-19 16:10:43 -0600

    Create-ServicesEx

---
+++ Get-ServicesEx.ps1
@@ -0,0 +1,18 @@
+﻿<#
+.Synopsis
+        Gets a list of services (including Log On As User information)
+.Example
+        PS> Get-ServicesEx win
+
+        Name                                          StartName                                     State                                        
+        ----                                          ---------                                     -----                                        
+        WinDefend                                     LocalSystem                                   Running                                      
+        WinHttpAutoProxySvc                           NT AUTHORITY\LocalService                     Running                                      
+        Winmgmt                                       localSystem                                   Running                                      
+        WinRM                                         NT AUTHORITY\NetworkService                   Running
+#>
+function Get-ServicesEx() {
+	param($Match);
+
+    gwmi win32_service | select Name, StartName, State | ? { $_.Name -match $Match }
+}

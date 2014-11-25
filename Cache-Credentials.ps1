48b78b97fa4fbbe2171f5da92357887d5a73b0cf
Ben Vierck <me@xcud.com>
2014-11-25 15:06:55 -0600

    Set-CachedCredentials, Get-CachedCredentials

---
+++ Cache-Credentials.ps1
@@ -0,0 +1,11 @@
+﻿
+function Set-CachedCredentials() {
+    Read-Host -Prompt "Username" | Out-File $env:LocalAppData\username.txt
+    Read-Host -Prompt "Password" -AsSecureString | ConvertFrom-SecureString | Out-File $env:LocalAppData\encryptedPassword.txt
+}
+
+function Get-CachedCredentials() {
+    $securePassword = gc $env:LocalAppData\encryptedPassword.txt | ConvertTo-SecureString
+    $username = gc $env:LocalAppData\username.txt
+    Write-Output (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $securePassword)
+}

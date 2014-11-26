0bc47850e4a8d6a5c82f5ff9bf4950122ebb1036
Ben Vierck <me@xcud.com>
2014-11-25 18:47:16 -0600

    Allow named cached credentials

--- Cache-Credentials.ps1
+++ Cache-Credentials.ps1
@@ -1,11 +1,11 @@
 ﻿
-function Set-CachedCredentials() {
-    Read-Host -Prompt "Username" | Out-File $env:LocalAppData\username.txt
-    Read-Host -Prompt "Password" -AsSecureString | ConvertFrom-SecureString | Out-File $env:LocalAppData\encryptedPassword.txt
+function Set-CachedCredentials($name) {
+    Read-Host -Prompt "Username" | Out-File $env:LocalAppData\$name-username.txt
+    Read-Host -Prompt "Password" -AsSecureString | ConvertFrom-SecureString | Out-File $env:LocalAppData\$name-encryptedPassword.txt
 }
 
-function Get-CachedCredentials() {
-    $securePassword = gc $env:LocalAppData\encryptedPassword.txt | ConvertTo-SecureString
-    $username = gc $env:LocalAppData\username.txt
+function Get-CachedCredentials($name) {
+    $securePassword = gc $env:LocalAppData\$name-encryptedPassword.txt | ConvertTo-SecureString
+    $username = gc $env:LocalAppData\$name-username.txt
     Write-Output (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $securePassword)
 }

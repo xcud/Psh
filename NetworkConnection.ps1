6e2910f0451796b07889e7d5d37cc8824e71039e
Ben Vierck <me@xcud.com>
2014-12-17 10:12:10 -0600

    New-NetworkConnection, Test-SQLConnection
    "net use \\machinename" without specifying a share was a useful tool
    that New-PsDrive does not allow. Net-NetworkConnection does.
    
    Test-SQLConnection is a WIP

---
+++ NetworkConnection.ps1
@@ -0,0 +1,17 @@
+﻿# work in progress, make a net connection without mapping a drive
+Function New-NetworkConnection
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)] $Hostname,
+        [Parameter()] [System.Management.Automation.PSCredential] $Credential
+    );
+
+    if($Credential) { 
+        $username = $Credential.UserName 
+        $password = $Credential.GetNetworkCredential().Password
+    }
+    
+    $net = new-object -ComObject WScript.Network
+    $net.MapNetworkDrive($null, ("\\{0}" -f $Hostname), $false, $username, $password)
+}
---
+++ Test-SQLConnection.ps1
@@ -0,0 +1,26 @@
+﻿Function Test-SQLConnection
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$true)] $DatabaseInstance,
+        [Parameter(ParameterSetName="SQL", Mandatory=$true)] [System.Management.Automation.PSCredential] $Credential,
+        [Parameter(ParameterSetName="Windows")] [System.Management.Automation.SwitchParameter] $UserWindowsAuth
+    );
+
+    if($UserWindowsAuth.IsPresent) {
+        $connectionString = "Data Source={0};Integrated Security=true;Initial Catalog=master;Connect Timeout=3;" -f $DatabaseInstance
+    }
+    else {
+        $connectionString = "Data Source={0};Integrated Security=false;Initial Catalog=master;Connect Timeout=3;User={1};Password={2}" -f $DatabaseInstance, $Credential.UserName, $Credential.GetNetworkCredential().Password
+    }
+    $sqlConn = New-Object System.Data.SqlClient.SqlConnection $connectionString
+    $sqlConn.Open()
+    if ($sqlConn.State -eq 'Open')
+    {
+        $sqlConn.Close();
+        Write-Output $true
+    }
+    else {
+        Write-Output $false
+    }
+ }

e3debf9f92fe313f672b7ac7296898e9a21aec46
Ben Vierck <me@xcud.com>
2014-12-17 14:15:40 -0600

    Use Get-Credential instead of Read-Host -As...

--- Cache-Credentials.ps1
+++ Cache-Credentials.ps1
@@ -1,11 +1,12 @@
 ﻿
-function Set-CachedCredentials($name) {
-    Read-Host -Prompt "Username" | Out-File $env:LocalAppData\$name-username.txt
-    Read-Host -Prompt "Password" -AsSecureString | ConvertFrom-SecureString | Out-File $env:LocalAppData\$name-encryptedPassword.txt
+function Set-CachedCredential($name) {
+    $cred = Get-Credential
+    $cred.UserName | Out-File $env:LocalAppData\$name.username.txt
+    $cred.Password | ConvertFrom-SecureString | Out-File $env:LocalAppData\$name.password.encrypted.txt
 }
 
-function Get-CachedCredentials($name) {
-    $securePassword = gc $env:LocalAppData\$name-encryptedPassword.txt | ConvertTo-SecureString
-    $username = gc $env:LocalAppData\$name-username.txt
+function Get-CachedCredential($name) {
+    $securePassword = gc $env:LocalAppData\$name.password.encrypted.txt | ConvertTo-SecureString
+    $username = gc $env:LocalAppData\$name.username.txt
     Write-Output (New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $securePassword)
 }

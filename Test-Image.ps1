18c3b62f6536cf9cbbab553a05fb6d603a820a96
Ben Vierck <me@xcud.com>
2015-01-29 14:07:57 -0600

    Friendly for pass-thru

--- Test-Image.ps1
+++ Test-Image.ps1
@@ -11,31 +11,47 @@
 function Test-Image {
     [CmdletBinding()]
     param(
-        [parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)] $Path
+        [parameter(Mandatory=$true, Position=0, ValueFromPipeline=$true)] 
+        $Path
     )
 
-    $knownImageHeaders = @{
-        jpg = @( "FF", "D8" );
-        bmp = @( "42", "4D" );
-        gif = @( "47", "49", "46" );
-        tif = @( "49", "49", "2A" );
-        png = @( "89", "50", "4E", "47", "0D", "0A", "1A", "0A" );
-    }
-
-    $bytes = Get-Content $path -Encoding Byte -ReadCount 1 -TotalCount 8
-
-    $knownImageHeaders.Keys | %{ 
-        # transform into array of the same length and format as what we're comparing against
-        $byteArray = ($bytes | select -first $knownImageHeaders[$_].Length | % { $_.ToString("X2") }) 
-        
-        # compare the two arrays
-        $diff = Compare-Object -ReferenceObject $knownImageHeaders[$_] -DifferenceObject $byteArray
-        if(($diff | Measure-Object).Count -eq 0)
-        {
-            Write-Output $true
-            break
+    PROCESS {
+        $knownImageHeaders = @{
+            jpg = @( "FF", "D8" );
+            bmp = @( "42", "4D" );
+            gif = @( "47", "49", "46" );
+            tif = @( "49", "49", "2A" );
+            png = @( "89", "50", "4E", "47", "0D", "0A", "1A", "0A" );
         }
-    }
 
-    Write-Output $false
+        $bytes = Get-Content $path -Encoding Byte -ReadCount 1 -TotalCount 8 -ErrorAction 0
+
+        $retval = $false
+        foreach($key in $knownImageHeaders.Keys) {
+            $knownHeaderArray = $knownImageHeaders[$key]
+
+            # transform into array of the same length and format as what we're comparing against
+            $byteArray = ($bytes | select -first $knownHeaderArray.Length | % { $_.ToString("X2") }) 
+            if($byteArray -eq $null) {
+                continue
+            }
+
+            # compare the two arrays
+            $diff = Compare-Object -ReferenceObject $knownHeaderArray -DifferenceObject $byteArray
+            if(($diff | Measure-Object).Count -eq 0) {
+                $retval = $true
+            }
+        }
+
+        return $retval
+    }
+}
+
+function Get-ChildImage {
+ [CmdletBinding()]
+    param(
+        [parameter(Mandatory=$false, Position=0, ValueFromPipeline=$true)] $Path
+    )
+
+    Get-ChildItem $Path | ? { Test-Image $_.FullName }
 }
